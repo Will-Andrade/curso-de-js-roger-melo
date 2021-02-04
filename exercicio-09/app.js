aad41858d9e8c59b83f5d7c11bad7d7f6d76baf7
Will-Andrade <willian.santos.andra@outlook.com>
2021-02-04 14:28:30 -0400

    Exercícios concluídos

--- exercicio-09/app.js
+++ exercicio-09/app.js
@@ -13,9 +13,13 @@
     valor no console.
 */
 
-function convertToString (value) {
-  return String(value)
-}
+// function convertToString (value) {
+//   return String(value)
+// }
+
+const convertToString = value => String(value);
+
+// console.log(typeof convertToString(200.923));
 
 /*
   02
@@ -23,6 +27,10 @@ function convertToString (value) {
   - Crie uma função que retorne a quantidade de caracteres que uma string  
     recebida por parâmetro possui.
 */
+
+const stringLength = string => string.length;
+
+// console.log(stringLength("Willian Bonni dos Santos Andrade"));
 
 /*
   03
@@ -34,12 +42,21 @@ function convertToString (value) {
   "CHOCOTONE E OVO DE PÁSCOA JUNTOS NO MERCADO EM PLENO FEVEREIRO"
 */
 
+const stringToLowerCase = string => string.toLowerCase();
+const testString = "CHOCOTONE E OVO DE PÁSCOA JUNTOS NO MERCADO EM PLENO FEVEREIRO";
+
+// console.log(stringToLowerCase(testString));
+
 /*
   04
 
   - Crie uma função que recebe 2 parâmetros: um caractere e uma string;
   - Ao ser invocada, a função deve retornar o index do caractere na string.
 */
+
+const indexOfCharacter = (character, string) => string.indexOf(character);
+
+// console.log(indexOfCharacter('e', 'Dark Knight Rises'));
 
 /*
   05
@@ -48,12 +65,20 @@ function convertToString (value) {
     passado por argumento existe no array (também passado por argumento).
 */
 
+const doesItIncludesItem = (element, array) => array.includes(element);
+
+// console.log(doesItIncludesItem(20, [10, 2, 0, 100, 22, 20]));
+
 /*
   06
 
   - Crie uma função que retorna a concatenação de 2 arrays, passados como  
     argumentos em sua invocação;
 */
+
+const concatArrays = (firstArray, secondArray) => firstArray.concat(secondArray);
+
+// console.log(concatArrays([20, 10, 20, 230, 40, 1], [20, 34, 35, 456]));
 
 /*
   07
@@ -62,12 +87,26 @@ function convertToString (value) {
     mas com o último item removido.
 */
 
+const popItem = array => {
+  array.pop();
+
+  return array
+}
+
+// console.log(popItem([100, 99, 98, 97, 96, 95]));
+
 /*
   08
 
   - Crie uma função que retorna se o valor passado como argumento em sua  
     invocação é null.
 */
+
+const isValueNull = (value) => value === null;
+
+// console.log(isValueNull(null));
+// console.log(isValueNull(''));
+// console.log(isValueNull(20));
 
 /*
   09
@@ -80,6 +119,11 @@ function convertToString (value) {
     foi exibido.
 */
 
+const invokeCallback = callback => callback();
+const showName = () => console.log('Willian Bonni dos Santos Andrade');
+
+// invokeCallback(showName);
+
 /*
   10
 
@@ -91,6 +135,12 @@ function convertToString (value) {
     resulte no triplo de 33.
 */
 
+const getTripleFunction = (value, callback) => callback(value);
+
+const theTriple = number => number * 3;
+
+// console.log(getTripleFunction(33, theTriple));
+
 /*
   11
 
@@ -100,7 +150,11 @@ function convertToString (value) {
   "O Xº item do array [X, X, X] é X."
 */
 
-const numbers = [1, 2, 3]
+const numbers = [1, 2, 3];
+
+// numbers.forEach((item, index, array) => {
+//   console.log(`O ${index + 1}º item do array ${array} é ${item}.`);
+// })
 
 /*
   12
@@ -113,9 +167,15 @@ const numbers = [1, 2, 3]
 const letters = ['v', 'e', 'p']
 let lettersCopy = []
 
-for (let i = 0; i < letters.length; i++) {
-  lettersCopy.push(letters[i])
-}
+// for (let i = 0; i < letters.length; i++) {
+//   lettersCopy.push(letters[i])
+// }
+
+letters.forEach((item) => {
+  lettersCopy.push(item);
+})
+
+// console.log(lettersCopy);
 
 /*
   13
@@ -142,11 +202,15 @@ const review = [
   'O primeiro filme foi baseado nesse livro, porém o livro (como sempre) é muito mais completo, com mais personagens, mais acontecimentos e até mesmo mais dinossauros. Na verdade nesse livro tem coisas do segundo e terceiro filme também, eles mudaram bastante nos filmes, acho que pra ficar mais comercial, e se o filme é bom, o livro é 100 vezes melhor.',
   'Michael é um ótimo autor, esse sim pesquisa muito antes de escrever um livro, além da história que já prende sua atenção, ele fala bastante de genética (pra explicar como os dinossauros foram criados) e acaba falando um pouco de programação (informática), por causa dos programas avançados e modernos que o parque tinha. E isso foi uma das coisas que eu achei muito legal, ele explica as coisas com gráficos, tabelas, códigos ... enfim, o cara é foda hahaha.',
   'Recomendo esse livro pra quem curte uma boa história de ficção. Apesar de muita gente pensar que o livro não tem graça, porque o legal mesmo é ver o dinossauro no filme, com todos os efeitos especiais, eu digo pra deixar esse pensamento de lado, pois a história é tão bem contada e os detalhes são tão bem relatados, que você passa a fazer parte da história, e vive todas as emoções hahaha.'
-]
+];
 
-let paragraphs = ''
+let paragraphs = '';
 
-section.innerHTML = paragraphs
+review.forEach((message) => {
+  paragraphs += `<p>${message}</p>`;
+})
+
+section.innerHTML = paragraphs;
 
 /*
   14
@@ -168,3 +232,30 @@ section.innerHTML = paragraphs
     pelo restante da quantidade de pessoas que curtiram o post (além das duas  
     pessoas já mencionadas no início da mensagem).
 */
+
+const postLikes = (thoseWhoLikedIt = []) => {
+  const amountOfLikes = thoseWhoLikedIt.length;
+  const firstName = thoseWhoLikedIt[0];
+  const secondName = thoseWhoLikedIt[1];
+  const thirdName = thoseWhoLikedIt[2];
+  const namesLeft = thoseWhoLikedIt.length - 2;
+
+  switch (amountOfLikes) {
+    case 0:
+      return "Ninguém curtiu isso."
+    case 1:
+      return `${firstName} curtiu isso.`
+    case 2:
+      return `${thoseWhoLikedIt.join(' e ')} curtiram isso.`
+    case 3:
+      return `${firstName}, ${secondName} e ${thirdName} curtiram isso.`
+    default:
+      return `${firstName}, ${secondName} e mais ${namesLeft} pessoas curtiram isso.`
+  }
+}
+
+// console.log(postLikes([]));
+// console.log(postLikes(['José']));
+// console.log(postLikes(['Jonas', 'Leonardo']));
+// console.log(postLikes(['Gabrielly', 'Fernanda', 'Viviane']))
+// console.log(postLikes(['Willian', 'Mateus', 'Thalys', 'Gustavo', 'Jonas']));

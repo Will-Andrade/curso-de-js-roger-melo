fd957e42a78d6cbd267a12032d058cfeef533956
Will-Andrade <willian.santos.andra@outlook.com>
2021-02-05 13:40:27 -0400

    Exercícios concluídos e refatorados.

--- exercicio-13/app.js
+++ exercicio-13/app.js
@@ -9,10 +9,16 @@
 */
 
 const getCatInfo = () => {
-  const name = 'Marcos'
-  let age = 3
-  const color = 'Cinza'
+  const name = 'Marcos';
+  let age = 3;
+  const color = 'Cinza';
+
+  return { name, age, color }
 }
+
+const { name, age, color } = getCatInfo();
+
+// console.log(`${name} é um gato ${color} de ${age} anos.`);
 
 /*
   02
@@ -23,16 +29,20 @@ const getCatInfo = () => {
 */
 
 const external = () => {
-  const movie = 'Parasite'
+  const movie = 'Parasite';
 
   const internal = () => {
     const extraInternal = () => {
-      console.log(movie.toUpperCase())
+      console.log(movie.toUpperCase());
     }
+
+    extraInternal();
   }
+
+  internal();
 }
 
-external()
+// external();
 
 /*
   03
@@ -43,7 +53,9 @@ external()
   Dica: procure pelo método reverse, no MDN.
 */
 
-let randomNumbers = [3, 2, 1]
+let randomNumbers = [3, 2, 1];
+
+// console.log(randomNumbers.reverse());
 
 /*
   04
@@ -57,7 +69,11 @@ let crazyArray = [
   { prop1: '1', prop2: '2' },
   function getMessage () { return 'hi' },
   [ 5, 96, 53  ]
-]
+];
+
+crazyArray.shift()
+
+// console.log(crazyArray);
 
 /*
   05
@@ -74,7 +90,11 @@ const dogs = [
   { name: 'Jade', age: 4, gender: 'Female', breed: 'Shiba inu' },
   { name: 'Zequinha', age: 7, gender: 'Male', breed: 'Poodle' },
   { name: 'Xica', age: 6, gender: 'Female', breed: 'Chihuahua' }
-]
+];
+
+const findDog = dogs.find(dog => dog.name === 'Zequinha');
+
+// console.log(findDog);
 
 /*
   06
@@ -108,6 +128,10 @@ const dogs = [
   </section>
 */
 
+const mainTitle = document.querySelector('.main-title');
+
+// console.log(mainTitle);
+
 /*
   07
 
@@ -115,3 +139,7 @@ const dogs = [
     página, através da classe deles;
   - Exiba esse NodeList no console.
 */
+
+const secondaryTitles = document.querySelectorAll('.secondary-title');
+
+// console.log(secondaryTitles);
